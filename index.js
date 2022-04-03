ee358202656aed538352e4030e96c65a262750b8
titanabrian <titanabrian.05@gmail.com>
2022-04-03 15:05:48 +0700

    add logging

--- index.js
+++ index.js
@@ -146,6 +146,7 @@ webServer.get('/lazynitip/product', async (req, res, next) => {
     }
     return res.json({url})
   } catch (e) {
+    console.log(e)
     return res.status(500).json({
       error_code: 'SOMETHING_WRONG',
       message: 'We investigate the issue'
